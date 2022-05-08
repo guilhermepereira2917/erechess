724274a55bd74a9103120d0edc802bea9be66c80
Guilherme Pereira <guilhermepereira2917@gmail.com>
2022-05-08 17:48:11 -0300

    Executavel do stockfish adicionado ao projeto, removendo a necessidade assim de manter seu executavel na variavel path do sistema

--- src/control/EngineUCI.java
+++ src/control/EngineUCI.java
@@ -2,13 +2,13 @@ package control;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import partida.model.Movimento;
 
 public class EngineUCI extends Thread {
 
@@ -23,7 +23,8 @@ public class EngineUCI extends Thread {
 
     public EngineUCI() {
         try {
-            engine = Runtime.getRuntime().exec("stockfish.exe");
+            String path = new File("src\\engines\\stockfish.exe").getAbsolutePath();
+            engine = Runtime.getRuntime().exec(path);
 
             input = engine.getInputStream();
             output = engine.getOutputStream();
